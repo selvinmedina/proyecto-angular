fc1520674b0469cba34bcdf05be62aa672d1920d
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-17 10:17:57 -0600

    Pagina sobrem mi, hecha

---
+++ src/app/components/about/about.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-about',
+  templateUrl: './about.component.html',
+  styleUrls: ['./about.component.css']
+})
+export class AboutComponent implements OnInit {
+  public title: string;
+  public subtitle: string;
+  public email: string;
+
+  constructor() {
+      this.title = 'Selvin Medina';
+      this.subtitle = 'Desarrollador de software';
+      this.email = 'selvinmedina0016@gmail.com';
+   }
+
+  ngOnInit() {
+  }
+
+}
