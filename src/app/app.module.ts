9082246f4bb20c7790fd23d3c87dfea50f5f377d
SelvinMedina <selvinmedina0016@gmail.com>
2019-10-18 00:01:31 -0600

    Comenzando el formulario

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import {routing, appRoutingProviders} from './app.routing';
+import {HttpClientModule} from '@angular/common/http';
+import {FormsModule} from '@angular/forms';
+
 
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
---
+++ src/app/services/project.service.ts
@@ -0,0 +1,23 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Project } from '../models/project';
+import { Global } from './global';
+
+@Injectable()
+export class ProjectService {
+    public url: string;
+
+    constructor(
+        private _htttp: HttpClient
+    ) {
+        this.url = Global.url;
+    }
+
+    testService() {
+        return 'Probando el servicio de Angular';
+    }
+}
+
+
+
