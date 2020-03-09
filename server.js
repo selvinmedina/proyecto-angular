427540468136e91ba50d5d23c6fa48364876b5e4
selvinmedina <selvinmedina0016@gmail.com>
2020-03-09 00:21:16 -0600

    c

--- server.js
+++ server.js
@@ -12,5 +12,4 @@ app.get('*', function (req, res) {
   // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname + '/dist/proyecto-angular/index.html'));
 });
-// Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 5000);
