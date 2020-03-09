2c3dfc5de554d0550df70843c80d3ee50cbb8159
selvinmedina <selvinmedina0016@gmail.com>
2020-03-09 00:25:15 -0600

    prod

--- server.js
+++ server.js
@@ -1,15 +1,11 @@
 //Install express server
-const express = require('express');
-const path = require('path');
+const express = require('express'),
+  path = require('path'),
+  app = express();
 
-const app = express();
-
-// Serve only the static files form the dist directory
-// Replace the '/dist/<to_your_project_name>'
 app.use(express.static(__dirname + '/dist/proyecto-angular'));
-
 app.get('*', function (req, res) {
-  // Replace the '/dist/<to_your_project_name>/index.html'
   res.sendFile(path.join(__dirname + '/dist/proyecto-angular/index.html'));
 });
+
 app.listen(process.env.PORT || 5000);
