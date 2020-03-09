149cf20b0c4218db1ad115605b698c0bf26f5d67
selvinmedina <selvinmedina0016@gmail.com>
2020-03-08 23:21:26 -0600

    tratando de hacer la distribucion

--- src/app/models/project.ts
+++ src/app/models/project.ts
@@ -1,11 +1,11 @@
 export class Project {
-    constructor(
-        public id: string,
-        public name: string,
-        public descripcion: string,
-        public category: string,
-        public year: number,
-        public langs: string,
-        public image: string
-    ) { }
+  constructor(
+    public _id: string,
+    public name: string,
+    public descripcion: string,
+    public category: string,
+    public year: number,
+    public langs: string,
+    public image: string
+  ) { }
 }
